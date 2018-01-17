f42ce6735ec5b01209d2e492b1538dbaf5a659d0
mtianyan <1147727180@qq.com>
2018-01-18 06:47:52 +0800

    完成伯乐在线的基本字段爬取，采用同步异步存入数据库。

--- ArticleSpider/utils/common.py
+++ ArticleSpider/utils/common.py
@@ -1,24 +1,17 @@
-# _*_ coding: utf-8 _*_
-import hashlib
-import re
-
+# encoding: utf-8
 __author__ = 'mtianyan'
-__date__ = '2017/5/22 20:56'
+__date__ = '2018/1/18 0018 03:52'
+import hashlib
+
 
 def get_md5(url):
-    #str就是unicode了
+    # str就是unicode了.Python3中的str对应2中的Unicode
     if isinstance(url, str):
         url = url.encode("utf-8")
     m = hashlib.md5()
     m.update(url)
     return m.hexdigest()
 
-def extract_num(text):
-    #从字符串中提取出数字
-    match_re = re.match(".*?(\d+).*", text)
-    if match_re:
-        nums = int(match_re.group(1))
-    else:
-        nums = 0
 
-    return nums
+if __name__ == "__main__":
+    print(get_md5("http://jobbole.com".encode("utf-8")))
